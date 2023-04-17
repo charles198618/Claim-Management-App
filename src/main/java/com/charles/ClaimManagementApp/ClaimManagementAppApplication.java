0fe890592f8e965f6a46d210bc7362777ad22eb2
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-04-17 13:10:20 -0400

    Initial commit

---
+++ src/main/java/com/charles/ClaimManagementApp/ClaimManagementAppApplication.java
@@ -0,0 +1,12 @@
+package com.charles.ClaimManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimManagementAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/charles/ClaimManagementApp/ClaimManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.ClaimManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
